e85511da8238f8ecf528ec59292ac57635a444eb
tamagosushio <mitachi.ts75@gmail.com>
2024-09-16 22:26:36 +0900

    [update] 文末の'.'を無視するように変更

--- server/controllers/controller.ts
+++ server/controllers/controller.ts
@@ -1,6 +1,7 @@
 import { helpers, RouterContext } from "https://deno.land/x/oak@v6.5.0/mod.ts";
 import { serveFile } from "https://deno.land/std@0.202.0/http/file_server.ts";
 import Tokipona from "./../models/tokipona.ts";
+import { Route } from "https://deno.land/x/oak@v17.0.0/mod.ts";
 
 Tokipona.init();
 
@@ -20,9 +21,15 @@ export const controller = {
     const requestJson = await ctx.request.body.json();
     const sentence: string = requestJson["sentence"];
     const meanList: { [key: string]: string }[] = [];
-    sentence.split(" ").forEach((word) => {
+    // 単語ごとにその意味を配列に詰める
+    sentence.split(" ").forEach((word, index, array) => {
+      // 最後の単語が"."で終わっている場合にはそれを取り除く
+      if(index === array.length-1 && word.endsWith(".")){
+        word = word.slice(0,-1);
+      }
       meanList.push(Tokipona.getMeanDict(word));
     });
+    // 対応しない単語があればエラー処理
     if (meanList.includes(undefined)) {
       ctx.response.status = 400;
       ctx.response.body = { message: "includes not existing word" };
@@ -32,4 +39,9 @@ export const controller = {
       ctx.response.body = JSON.stringify(responseJson, null, 2);
     }
   },
+
+  async getSentenceImage(ctx: RouterContext){
+    const requestJson = await ctx.request.body.json();
+    const sentence: string = requestJson["sentence"];
+  }
 };
