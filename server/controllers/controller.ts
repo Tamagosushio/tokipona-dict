a9dd0f22260702363910cc31be263977cf3a46c1
tamagosushi <mitachi.ts75@gmail.com>
2024-09-18 13:28:39 +0900

    [change] 画像生成をクライアントで行うように変更

--- server/controllers/controller.ts
+++ server/controllers/controller.ts
@@ -1,6 +1,5 @@
 import { helpers, RouterContext } from "https://deno.land/x/oak@v6.5.0/mod.ts";
 import Tokipona from "./../models/tokipona.ts";
-import makeImageBase64 from "../models/makeImageBase64.ts";
 
 Tokipona.init();
 const decoder = new TextDecoder();
@@ -19,14 +18,12 @@ export const controller = {
 
   async getMeanSentence(ctx: RouterContext) {
     const requestJson = await ctx.request.body.json();
-    const sentence: string = requestJson["sentence"];
+    let sentence: string = requestJson["sentence"];
+    sentence = sentence
+      .replaceAll(/\(/g,"").replaceAll(/\)/g,"").replaceAll(/\[/g,"").replaceAll(/\]/g,"").replaceAll(/\./g,"").replaceAll(/\:/g,"");
     const meanList: { [key: string]: string }[] = [];
     // 単語ごとにその意味を配列に詰める
-    sentence.split(" ").forEach((word, index, array) => {
-      // 最後の単語が"."で終わっている場合にはそれを取り除く
-      if (index === array.length - 1 && word.endsWith(".")) {
-        word = word.slice(0, -1);
-      }
+    sentence.split(" ").forEach((word) => {
       meanList.push(Tokipona.getMeanDict(word));
     });
     // 対応しない単語があればエラー処理
@@ -38,19 +35,5 @@ export const controller = {
       responseJson["sentenceMeans"] = meanList;
       ctx.response.body = JSON.stringify(responseJson, null, 2);
     }
-  },
-
-  async getSentenceImage(ctx: RouterContext) {
-    const requestJson = await ctx.request.body.json();
-    const sentence: string = requestJson["sentence"];
-    const imageBase64: string = makeImageBase64(sentence).toString();
-    if (imageBase64) {
-      const responseJson = {};
-      responseJson["image"] = imageBase64;
-      ctx.response.body = JSON.stringify(responseJson, null, 2);
-    } else {
-      ctx.response.status = 400;
-      ctx.response.body = { message: "failed to generate image" };
-    }
-  },
+  }
 };
